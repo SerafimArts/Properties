e5c6dfaa2deb54da715e27203f3958b7a59b3573
serewann <serewann@gmail.com>
2016-09-28 16:55:50 +0300

    Add properties type checking for writing a new value

--- src/Setters.php
+++ src/Setters.php
@@ -36,12 +36,12 @@ trait Setters
 
         $parser = Registry::get($this);
         if ($parser->has($name)) {
-            if ($parser->isWritable($name)) {
-                $this->setPropertyValue($name, $value);
-            } else {
-                $exception = sprintf('Can not set value to read only property %s::$%s', get_class($this), $name);
-                throw new \LogicException($exception);
-            }
+            if (!$parser->isWritable($name)) throw new \LogicException(
+                sprintf('Can not set value to read only property %s::$%s', get_class($this), $name));
+            if (!$parser->typesAreEqual($name, $value)) throw new \LogicException(
+                sprintf('Can not set value: types mismatch %s::$%s', get_class($this), $name));
+
+            $this->setPropertyValue($name, $value);
         }
 
         $this->$name = $value;
